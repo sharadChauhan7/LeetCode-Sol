461e310cf316dc25578aac4958d66bc7d7db8df3
Sharad Singh Chauhan <103198852+sharadChauhan7@users.noreply.github.com>
2024-07-23 17:00:42 +0530

    Time: 2 ms (42.10%) | Memory: 44.1 MB (86.55%) - LeetSync

---
+++ 46-permutations/permutations.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        List<Integer> curr = new ArrayList<>();
+        List<Integer> arr = new ArrayList<>();
+
+        // Add each element from the array to the List
+        for (int element : nums) {
+            arr.add(element);
+        }
+        helper(arr,curr,ans);
+        return ans;
+    }
+    public static void helper(List<Integer>nums ,List<Integer> curr,List<List<Integer>>ans){
+        if(nums.size()<=0){
+            ans.add(new ArrayList<>(curr));
+            return;
+        }
+
+        for(int i=0;i<nums.size();i++){
+            int num = nums.remove(i);
+            curr.add(num);
+            helper(nums,curr,ans);
+            nums.add(i,num);
+            curr.remove(curr.size()-1);
+        }
+        return;
+    }
+}
