f486656e7a4e7d55942ce68648d0bac609ff74dc
Sharad Singh Chauhan <103198852+sharadChauhan7@users.noreply.github.com>
2024-08-14 10:59:35 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (26.58%) - LeetSync

---
+++ 669-trim-a-binary-search-tree/trim-a-binary-search-tree.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode trimBST(TreeNode root, int L, int R) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val >= L && root.val <= R) {
+            root.left = trimBST(root.left, L, R);
+            root.right = trimBST(root.right, L, R);
+        } else if (root.val < L) {
+            root = trimBST(root.right, L, R);
+        } else if (root.val > R) {
+            root = trimBST(root.left, L, R);
+        }
+        return root;
+    }
+
+
+}
