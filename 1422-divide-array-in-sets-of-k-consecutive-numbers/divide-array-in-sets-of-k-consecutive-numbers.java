88f7ebb25dc78b3f5c58cbf2322d16dc16fd7787
Sharad Singh Chauhan <103198852+sharadChauhan7@users.noreply.github.com>
2024-09-25 22:23:42 +0530

    Time: 96 ms (64.88%) | Memory: 58.6 MB (58.64%) - LeetSync

--- 1422-divide-array-in-sets-of-k-consecutive-numbers/divide-array-in-sets-of-k-consecutive-numbers.java
+++ 1422-divide-array-in-sets-of-k-consecutive-numbers/divide-array-in-sets-of-k-consecutive-numbers.java
@@ -1,23 +1,27 @@
 class Solution {
     public boolean isPossibleDivide(int[] nums, int k) {
-        Arrays.sort(nums);
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int h:nums){
-           map.put(h,map.getOrDefault(h,0)+1);
+        if(nums.length%k!=0)return false;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i:nums){
+            map.put(i,map.getOrDefault(i,0)+1);
         }
-        for(int h:nums){
-            if(map.get(h)>0){
-                for(int j = 0;j<k;j++){
-                    int nexthand = h+j;
-                     if(map.containsKey(nexthand)&&map.get(nexthand)>0){
-                        map.put(nexthand,map.getOrDefault(nexthand,0)-1);
-                     }
-                     else{
-                        return false;
-                     }
+        PriorityQueue<Integer> pq =new PriorityQueue<>();
+        for(int i:map.keySet()){
+            pq.add(i);
+        }
+        while(!pq.isEmpty()){
+            int first = pq.peek();
+            for(int i=first;i<first+k;i++){
+                if(!map.containsKey(i))return false;
+                else{
+                    map.put(i,map.get(i)-1);
+                    if(map.get(i)==0){
+                        int val= pq.remove();
+                        if(val!=i)return false;
+                    }
                 }
             }
         }
-        return true; 
+        return true;
     }
 }
