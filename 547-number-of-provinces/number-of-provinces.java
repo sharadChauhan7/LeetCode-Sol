98ab699f74cc7c2a3e4ed07c4b75a97ab89cda36
Sharad Singh Chauhan <103198852+sharadChauhan7@users.noreply.github.com>
2024-10-15 21:44:08 +0530

    Time: 14 ms (5.32%) | Memory: 47.9 MB (28.45%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,27 +1,6 @@
 class Solution {
-    class Edge{
-        int src;
-        int dest;
-        public Edge(int src,int dest){
-            this.src = src;
-            this.dest= dest;
-        }
-    }
-    public ArrayList<Edge> [] buildGraph(int[][] isConnected){
 
-        ArrayList<Edge> [] graph = new ArrayList [isConnected.length];
-        for(int i=0;i<isConnected.length;i++){
-            ArrayList<Edge> temp = new ArrayList<>();
-            for(int j=0;j<isConnected.length;j++){
-                if(isConnected[i][j]==1){
-                    temp.add(new Edge(i,j));
-                }
-            }
-            graph[i]=temp;
-        }
-        return graph;
-    }
-        public static void bfsUtil(ArrayList<Edge>[] graph ,boolean [] visited,int starting){
+        public static void bfsUtil(int [][] graph ,boolean [] visited,int starting){
         Queue<Integer> q = new LinkedList<Integer>();
         // boolean visited [] = new boolean[graph.length];
         q.add(starting);
@@ -30,14 +9,19 @@ class Solution {
             if(!visited[curr]){
                 visited[curr]=true;
                 System.out.print(curr+" ");
-                for(int i=0;i<graph[curr].size();i++){
-                    q.add(graph[curr].get(i).dest);
+                // for(int i=0;i<graph[curr].size();i++){
+                //     q.add(graph[curr].get(i).dest);
+                // }
+                for(int i =0;i<graph.length;i++){
+                    if(graph[curr][i] == 1){
+                        q.add(i);
+                    }
                 }
             }
         }
         return;
     }
-    public static int bfs(ArrayList<Edge>[] graph,int starting){
+    public static int bfs(int[][] graph,int starting){
         boolean isVisited [] = new boolean[graph.length];
         int count=0;
         for(int i=0;i<graph.length;i++){
@@ -48,8 +32,7 @@ class Solution {
         }
         return count;
     }
-    public int findCircleNum(int[][] isConnected) {
-        ArrayList<Edge> graph [] = buildGraph(isConnected);
+    public int findCircleNum(int[][] graph) {
         return bfs(graph,0);
     }
 }
