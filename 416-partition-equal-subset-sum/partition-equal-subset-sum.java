81cc0b8a334dcf14a01964e62bbcdbb8546440bf
Sharad Singh Chauhan <103198852+sharadChauhan7@users.noreply.github.com>
2024-11-21 14:49:54 +0530

    Time: 63 ms (33.37%) | Memory: 74.9 MB (5.04%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,25 +1,23 @@
 class Solution {
     public boolean canPartition(int[] arr) {
-        int target =0;
-        for(int a :arr){
+        int target=0;
+        for(int a:arr){
             target+=a;
         }
-        if(target%2!=0){
-            return false;
-        }
+        if(target%2!=0)return false;
         int dp[][] = new int [arr.length+1][target+1];
         for(int num[]:dp){
             Arrays.fill(num,-1);
         }
-        return isSubsetSumUtil(arr,target,0,dp,target);
+        return isSubsetSumUtil(arr,target/2,0,dp);
     }
-    public boolean isSubsetSumUtil(int arr[], int target,int idx,int dp[][],int total){
+    public boolean isSubsetSumUtil(int arr[], int target,int idx,int dp[][]){
         
-        if(target*2==total){
+        if(target==0){
             return true;
         }
         
-        if(target<total/2){
+        if(target<0){
             return false;
         }
         
@@ -30,7 +28,7 @@ class Solution {
             return dp[idx][target]==1?true:false;
         }
         
-        boolean ans = isSubsetSumUtil(arr,target,idx+1,dp,total) || isSubsetSumUtil(arr, target-arr[idx], idx+1,dp,total);
+        boolean ans = isSubsetSumUtil(arr,target,idx+1,dp) || isSubsetSumUtil(arr, target-arr[idx], idx+1,dp);
         dp[idx][target]=ans?1:0;
         return ans;
         
