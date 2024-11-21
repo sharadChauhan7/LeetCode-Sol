46db2c7d9ff13a620092129520221f6e9fd6d12e
Sharad Singh Chauhan <103198852+sharadChauhan7@users.noreply.github.com>
2024-11-21 14:44:11 +0530

    Time: 127 ms (10.41%) | Memory: 71.6 MB (8.62%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean canPartition(int[] arr) {
+        int target =0;
+        for(int a :arr){
+            target+=a;
+        }
+        int dp[][] = new int [arr.length+1][target+1];
+        for(int num[]:dp){
+            Arrays.fill(num,-1);
+        }
+        return isSubsetSumUtil(arr,target,0,dp,target);
+    }
+    public boolean isSubsetSumUtil(int arr[], int target,int idx,int dp[][],int total){
+        
+        if(target*2==total){
+            return true;
+        }
+        
+        if(target<total/2){
+            return false;
+        }
+        
+        if(idx>=arr.length){
+            return false;
+        }
+        if(dp[idx][target]!=-1){
+            return dp[idx][target]==1?true:false;
+        }
+        
+        boolean ans = isSubsetSumUtil(arr,target,idx+1,dp,total) || isSubsetSumUtil(arr, target-arr[idx], idx+1,dp,total);
+        dp[idx][target]=ans?1:0;
+        return ans;
+        
+        
+    }
+}
