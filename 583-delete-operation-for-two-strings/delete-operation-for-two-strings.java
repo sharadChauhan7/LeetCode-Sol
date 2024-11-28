6a3985b143305d30be1a59622b518e0aa658579f
Sharad Singh Chauhan <103198852+sharadChauhan7@users.noreply.github.com>
2024-11-28 21:12:08 +0530

    Time: 7 ms (82.70%) | Memory: 44.7 MB (55.86%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -1,10 +1,30 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int dp[][] = new int [word1.length()][word2.length()];
-        for(int arr[]:dp){
-            Arrays.fill(arr,-1);
+        int dp[][] = new int [word1.length()+1][word2.length()+1];
+        int n = word1.length();
+        int m = word2.length();
+        for(int i=n;i>=0;i--){
+            dp[i][m]=n-i;
         }
-        return minDistanceUtil(word1,word2,0,0,dp); 
+        for(int i=m;i>=0;i--){
+            dp[n][i]=m-i;
+        }
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                int ans =0;
+                if(word1.charAt(i)==word2.charAt(j)){
+                    ans =dp[i+1][j+1];
+                }
+                else{
+                    int l = dp[i+1][j];
+                    int r = dp[i][j+1];
+                    ans = 1+Math.min(l,r);
+                }
+                dp[i][j]=ans;
+            }
+        }
+        return dp[0][0];
     }
 
     public int minDistanceUtil(String word1, String word2,int i,int j,int [][]dp){
@@ -26,7 +46,6 @@ class Solution {
         else{
             int l = minDistanceUtil(word1,word2,i+1,j,dp);
             int r = minDistanceUtil(word1,word2,i,j+1,dp);
-
             ans = 1+Math.min(l,r);
         }
         return dp[i][j]=ans;
