e29bbc3297d23b5f553d4841ccfe5c47f07938c7
Sharad Singh Chauhan <103198852+sharadChauhan7@users.noreply.github.com>
2024-12-01 23:55:41 +0530

    Time: 20 ms (77.75%) | Memory: 51 MB (24.02%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,44 +1,40 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        // int dp[][] = new int[text1.length()][text2.length()];
-        // for(int []num:dp){
-        //     Arrays.fill(num,-1);
-        // }
-        // return helper(text1,text2,0,0,dp);
+        int n= text1.length();
+        int m = text2.length();
+        int dp[][] = new int [n+1][m+1];
 
-        return lcsBU(text1,text2);
-    }
-
-    public int helper(String s1, String s2 , int i, int j,int dp[][]){
-        if(i>=s1.length() || j>=s2.length()){
-            return 0;
-        }
-        if(dp[i][j]!=-1)return dp[i][j];
-        int ans =0;
-        if(s1.charAt(i) == s2.charAt(j)){
-            ans=1+helper(s1,s2,i+1,j+1,dp);
-            
-        }
-        else{
-            int l = helper(s1,s2,i+1,j,dp);
-            int r = helper(s1,s2,i,j+1,dp);
-            ans += Math.max(l,r);
-        }
-        return dp[i][j]=ans;
-    }
-    public int lcsBU(String s1, String s2 ){
-        int dp[][] = new int [s1.length()+1][s2.length()+1];
-        for(int i=1;i<dp.length;i++){
-            for(int j=1;j<dp[0].length;j++){
-                if(s1.charAt(i-1)==s2.charAt(j-1)){
-                    dp[i][j] = 1+dp[i-1][j-1];
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                int ans =0;
+                if(text1.charAt(i)==text2.charAt(j)){
+                    dp[i][j]=1+dp[i+1][j+1];
                 }
                 else{
-                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                    int l = dp[i][j+1];
+                    int r = dp[i+1][j];
+                    dp[i][j]=Math.max(l,r);
                 }
             }
         }
-        return dp[s1.length()][s2.length()];
+        return dp[0][0];
+
     }
 
+    public int helper(String text1, String text2 ,int i,int j,int dp[][]){
+        if(i>=text1.length() || j>=text2.length()){
+            return 0;
+        }
+        int ans =0;
+        if(text1.charAt(i)==text2.charAt(j)){
+            ans=1+helper(text1,text2,i+1,j+1,dp);
+        }
+        else{
+            int l = helper(text1,text2,i,j+1,dp);
+            int r = helper(text1,text2,i+1,j,dp);
+            ans+=Math.max(l,r);
+        }
+
+        return ans;
+    }
 }
