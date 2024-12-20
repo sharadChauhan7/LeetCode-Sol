818670fcebd11c356c685860377d9d91ceafb2b8
Sharad Singh Chauhan <103198852+sharadChauhan7@users.noreply.github.com>
2024-12-20 19:43:54 +0530

    Time: 1 ms (82.86%) | Memory: 42.1 MB (20.27%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,44 +1,35 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int dp[][][] = new int [prices.length][2][2];
-        for(int arr[][]:dp){
-            for(int ar[]:arr){
-                Arrays.fill(ar,-1);
-            }
+        int dp [][] = new int [prices.length][2];
+        for(int arr[]:dp){
+            Arrays.fill(arr,-1);
         }
-        return maxProfitUtil(prices,0,1,1,dp);
+        return maxProfitUtil(prices,0,1,dp);
     }
-    public int maxProfitUtil(int []prices,int idx,int flag,int cooldown,int dp[][][]){
+    public int maxProfitUtil(int []prices,int idx,int flag,int dp[][]){
         if(idx>=prices.length){
             return 0;
         }
         // Choices
         int ans =0;
-        if(dp[idx][flag][cooldown]!=-1){
-            return dp[idx][flag][cooldown];
+        if(dp[idx][flag]!=-1){
+            return dp[idx][flag];
         }
-
         if(flag==1){
 
-            if(cooldown==1){
-                int buy = maxProfitUtil(prices,idx+1,0,1,dp);
+            int buy = maxProfitUtil(prices,idx+1,0,dp);
 
-                int notBuy  = maxProfitUtil(prices,idx+1,1,1,dp);
-                ans = Math.max(buy-prices[idx],notBuy);
-            }
-            else{
-                int notBuy = maxProfitUtil(prices,idx+1,1,1,dp);
-                ans = notBuy;
-            }
+            int notBuy  = maxProfitUtil(prices,idx+1,1,dp);
 
+            ans = Math.max(buy-prices[idx],notBuy);
         }
         else{
-            int sell = maxProfitUtil(prices,idx+1,1,0,dp);
+            int sell = maxProfitUtil(prices,idx+2,1,dp);
 
-            int notSell  = maxProfitUtil(prices,idx+1,0,1,dp);
+            int notSell  = maxProfitUtil(prices,idx+1,0,dp);
 
             ans = Math.max(sell+prices[idx],notSell);
         }
-        return dp[idx][flag][cooldown]= ans;
+        return dp[idx][flag]= ans;
     }
 }
