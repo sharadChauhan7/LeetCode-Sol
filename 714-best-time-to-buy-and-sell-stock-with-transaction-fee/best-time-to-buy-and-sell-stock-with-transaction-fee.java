84342f0d16962be0ae8f284608397ae9829ceda6
Sharad Singh Chauhan <103198852+sharadChauhan7@users.noreply.github.com>
2024-12-20 19:54:28 +0530

    Time: 18 ms (48.57%) | Memory: 55 MB (78.30%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,10 +1,23 @@
 class Solution {
     public int maxProfit(int[] prices, int fee) {
-        int dp[][] = new int [prices.length][2];
-        for(int arr[]:dp){
-            Arrays.fill(arr,-1);
+        int dp[][] = new int [prices.length+1][2];
+        for(int i=prices.length-1;i>=0;i--){
+            for(int j=1;j>=0;j--){
+                int ans=0;
+                if(j==1){
+                    int take = dp[i+1][0]-prices[i];
+                    int notTake = dp[i+1][1];
+                    ans = Math.max(take,notTake);
+                }
+                else{
+                    int take = dp[i+1][1]+prices[i]-fee;
+                    int notTake = dp[i+1][0];
+                    ans = Math.max(take,notTake);
+                }
+                dp[i][j]= ans;
+            }
         }
-        return maxProfitUtil(prices,fee,0,1,dp);
+        return dp[0][1];
     }
     public int maxProfitUtil(int []prices,int fee,int idx,int flag,int dp[][]){
         if(idx>=prices.length){
