a7a66cb617939ab37e876b2d9d2580951c335dd5
Sharad Singh Chauhan <103198852+sharadChauhan7@users.noreply.github.com>
2025-01-05 00:59:07 +0530

    Time: 1 ms (81.21%) | Memory: 41.6 MB (76.42%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,32 +1,25 @@
 class Solution {
     public int numDecodings(String s) {
-        int dp[] = new int[s.length()];
-        Arrays.fill(dp,-1);
-        return decode(s,0,dp);
-    }
+        int dp[] = new int[2];
 
-    public int decode(String s,int idx,int []dp){
-        if(idx >=s.length()){
-            return 1;
-        }
-
-        // One
-        if(dp[idx]!=-1){
-            return dp[idx];
-        }
-        int ans =0;
-        int one=0,two=0;
-        if(s.charAt(idx)-48!=0){
-            one = decode(s,idx+1,dp);
-        }
-        // Two
-        if(idx+1<s.length() && s.charAt(idx)!='0'){
-            int newNum =  (s.charAt(idx)-48)*10+(s.charAt(idx+1)-48);
-            if(newNum>9 && newNum <27){
-                two = decode(s,idx+2,dp);
+        dp[0] = 1;
+        for(int idx = s.length()-1;idx>=0;idx--){
+            int ans =0;
+            int one=0,two=0;
+            
+            if(s.charAt(idx)-48!=0){
+                one = dp[0];
             }
+            // Two
+            if(idx+1<s.length() && s.charAt(idx)!='0'){
+                int newNum =  (s.charAt(idx)-48)*10+(s.charAt(idx+1)-48);
+                if(newNum>9 && newNum <27){
+                    two = dp[1];
+                }
+            }
+            dp[1]=dp[0];
+            dp[0]=one+two;
         }
-        return dp[idx]=one+two;
-
+        return dp[0];
     }
 }
