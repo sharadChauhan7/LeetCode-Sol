17cc6125585d5b5e635558008a8d3d34dd5aa079
Sharad Singh Chauhan <103198852+sharadChauhan7@users.noreply.github.com>
2025-01-04 23:53:44 +0530

    Time: 2438 ms (4.23%) | Memory: 48.5 MB (5.03%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
@@ -1,10 +1,30 @@
 class Solution {
     public int mincostTickets(int[] days, int[] costs) {
-        int dp[][] = new int [days.length][397];
-        for(int arr[]:dp){
-            Arrays.fill(arr,-1);
+        int dp[][] = new int [days.length+1][days[days.length-1]+31];
+
+        for(int idx = days.length-1;idx>=0;idx--){
+            for(int dday =days[days.length-1]+30 ;dday>=1;dday--){
+                int ans =0;
+                if(days[idx]>=dday){
+                int c1 = dp[idx+1][days[idx]+1]+costs[0];
+                int c2 = dp[idx+1][days[idx]+7]+costs[1];
+                int c3 = dp[idx+1][days[idx]+30]+costs[2];
+
+                ans = Math.min(c1,Math.min(c2,c3));
+                }
+                else{
+                    ans = dp[idx+1][dday];
+                }
+                dp[idx][dday]=ans;
+            }
         }
-        return mT(days,costs,0,0,dp);
+        for(int arr[]:dp){
+            for(int a:arr){
+                System.out.print(a+" ");
+            }
+            System.out.println();
+        }
+        return dp[0][1];
     }
     public int mT(int[] days, int[] costs,int dday,int idx,int [][]dp){
         if(idx >=days.length){
