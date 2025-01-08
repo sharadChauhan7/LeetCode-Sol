09cefc5e5746760bb8b7573a9c36863f5077dcb0
Sharad Singh Chauhan <103198852+sharadChauhan7@users.noreply.github.com>
2025-01-08 23:15:29 +0530

    Time: 1 ms (100.00%) | Memory: 42.6 MB (35.80%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -2,37 +2,35 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        boolean [][] isV = new boolean[n][m];
-        Queue<int[]> q = new LinkedList<>();
+        boolean isV[][] = new boolean[n][m];
+        Queue<int []> q = new LinkedList<>();
+
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
-                    isV[i][j]= true;
-                    q.add(new int[]{i,j});
+                    q.add(new int []{i,j});
                 }
             }
         }
-        q.add(new int []{-1,-1});
-        int time =-1;
-        int dir[][] = {{-1,0},{0,-1},{1,0},{0,1}};
+        // System.out.println(q.peek()[0]);
+        q.add(new int[]{-1,-1});
+        int count =0;
+        int [][] dir ={{1,0},{-1,0},{0,1},{0,-1}};
         while(!q.isEmpty()){
-            int curr[] = q.remove();
+            int [] curr = q.remove();
             if(curr[0]==-1){
-                time++;
                 if(q.isEmpty()){
                     break;
                 }
+                count++;
                 q.add(curr);
-                continue;
             }
-
-            for(int i=0;i<4;i++){
-                int r = curr[0]+dir[i][0];
-                int c = curr[1]+dir[i][1];
-                if(r >=0 && r <n && c >=0 && c <m){
-                    if(grid[r][c]==1 && !isV[r][c]){
-                        isV[r][c]=true;
-                        grid[r][c] = 2;
+            else{
+                for(int i=0;i<4;i++){
+                    int r = curr[0]+dir[i][0];
+                    int c = curr[1]+dir[i][1];
+                    if(r<n && r>=0 && c<m && c>=0 && grid[r][c]==1){
+                        grid[r][c]=2;
                         q.add(new int []{r,c});
                     }
                 }
@@ -41,10 +39,10 @@ class Solution {
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==1){
-                    return -1;
+                    count =-1;
                 }
             }
         }
-        return time;
+        return count;
     }
 }
