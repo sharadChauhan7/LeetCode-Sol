ce7a1478646b83c7815aeb8819b6d3e8f81e40df
Sharad Singh Chauhan <103198852+sharadChauhan7@users.noreply.github.com>
2025-01-20 23:06:14 +0530

    Time: 11 ms (14.39%) | Memory: 45.6 MB (41.63%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,37 +1,33 @@
 class Solution {
+    static int counter=0;
     public boolean canFinish(int numCourses, int[][] pr) {
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         for(int i=0;i<numCourses;i++){
             adj.add(new ArrayList<Integer>());
         }
+        int inDegree[] = new int [adj.size()];
         for(int i=0;i<pr.length;i++){
             adj.get(pr[i][0]).add(pr[i][1]);
+            inDegree[pr[i][1]]++;
         }
         System.out.println(adj);
         // return false;
         Queue<Integer> q = new LinkedList<>();
-        int inDegree[] = new int [adj.size()];
-        
-        for(int i=0;i<adj.size();i++){
-            for(int j =0;j<adj.get(i).size();j++){
-                int neigh = adj.get(i).get(j);
-                inDegree[neigh]++;
-            }
-        }
-        ArrayList<Integer> ans = new ArrayList<>();
         
 
+            counter=0;
         for(int i=0;i<adj.size();i++){
            if(inDegree[i]==0){
                q.add(i);
-               ans.add(i);
+               counter++;
            }
         }
-            tsUtil(adj,q,inDegree,ans);
+        
+            tsUtil(adj,q,inDegree);
             
-        return ans.size()==adj.size()?true:false;
+        return counter==adj.size()?true:false;
     }
-    static void tsUtil(ArrayList<ArrayList<Integer>> adj,Queue<Integer>q,int inDegree[],ArrayList<Integer>ans){
+    static void tsUtil(ArrayList<ArrayList<Integer>> adj,Queue<Integer>q,int inDegree[]){
         
         while(!q.isEmpty()){
             int curr = q.remove();
@@ -41,7 +37,7 @@ class Solution {
                 inDegree[neigh]--;
                 if(inDegree[neigh]==0){
                     q.add(neigh);
-                    ans.add(neigh);
+                    counter++;
                 }
             }
         }
