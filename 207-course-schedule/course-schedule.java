7b08b91c7b0cf048b2640225fe74df84553a1f70
Sharad Singh Chauhan <103198852+sharadChauhan7@users.noreply.github.com>
2025-03-30 17:52:20 +0530

    Time: 7 ms (51.63%) | Memory: 45.5 MB (57.30%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,46 +1,29 @@
 class Solution {
-    static int counter=0;
-    public boolean canFinish(int numCourses, int[][] pr) {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int [] indegree = new int[numCourses];
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         for(int i=0;i<numCourses;i++){
-            adj.add(new ArrayList<Integer>());
+            adj.add(new ArrayList<>());
         }
-        int inDegree[] = new int [adj.size()];
-        for(int i=0;i<pr.length;i++){
-            adj.get(pr[i][0]).add(pr[i][1]);
-            inDegree[pr[i][1]]++;
-        }
-        System.out.println(adj);
-        // return false;
-        Queue<Integer> q = new LinkedList<>();
-        
-
-            counter=0;
-        for(int i=0;i<adj.size();i++){
-           if(inDegree[i]==0){
-               q.add(i);
-               counter++;
-           }
-        }
-        
-            tsUtil(adj,q,inDegree);
+        for(int i=0;i<prerequisites.length;i++){
             
-        return counter==adj.size()?true:false;
-    }
-    static void tsUtil(ArrayList<ArrayList<Integer>> adj,Queue<Integer>q,int inDegree[]){
-        
+            adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
+            indegree[prerequisites[i][0]]++;
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for(int i =0;i<numCourses;i++){
+            if(indegree[i]==0) q.add(i);
+        }
+        int count =0;
         while(!q.isEmpty()){
             int curr = q.remove();
-        
+            count++;
             for(int i=0;i<adj.get(curr).size();i++){
                 int neigh = adj.get(curr).get(i);
-                inDegree[neigh]--;
-                if(inDegree[neigh]==0){
-                    q.add(neigh);
-                    counter++;
-                }
+                indegree[neigh]--;
+                if(indegree[neigh]==0) q.add(neigh);
             }
         }
-        return;
+        return count==numCourses;
     }
 }
