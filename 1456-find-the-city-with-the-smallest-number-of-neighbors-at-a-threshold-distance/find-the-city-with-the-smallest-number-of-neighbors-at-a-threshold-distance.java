ceae5f4840092734bcd6d78580aa508b7519189c
Sharad Singh Chauhan <103198852+sharadChauhan7@users.noreply.github.com>
2025-04-11 13:52:57 +0530

    Time: 9 ms (77.75%) | Memory: 44.2 MB (92.79%) - LeetSync

---
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        int city[][] = new int [n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i == j){
+                    city[i][j]=0;
+                }
+                else{
+                    city[i][j]=(int)1e9;
+                }
+            }
+        }
+        for(int i=0;i<edges.length;i++){
+            int r = edges[i][0];
+            int c = edges[i][1];
+            // System.out.println(r+" "+c+" "+edges[i][2]+" ");
+            city[r][c] = edges[i][2]; 
+            city[c][r] = edges[i][2];
+        }
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         System.out.print(city[i][j]+" ");
+        //     }
+        //     System.out.println();
+        // }
+        int ans=0;
+        int anscount=Integer.MAX_VALUE;
+        for(int via = 0;via<n;via++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    // System.out.print(city[i][j]+" ");
+                    city[i][j] = Math.min(city[i][j],(city[i][via]+city[via][j]));
+                    
+
+                    // System.out.print(city[i][j]);     
+                }
+                // System.out.println();
+            }
+        }
+        for(int i=0;i<n;i++){
+            int count=0;
+            for(int j=0;j<n;j++){
+                if(city[i][j]<=distanceThreshold){
+                        count++;
+                }
+            }
+            if(count<=anscount){
+                ans=i;
+                anscount=count;
+            }
+        }
+        return ans;
+    }
+}
