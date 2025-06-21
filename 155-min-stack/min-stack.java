2ed5ff42e323c843ded8686f285abb3bbd9a4a13
Sharad Singh Chauhan <103198852+sharadChauhan7@users.noreply.github.com>
2025-06-21 08:26:13 +0530

    Time: 4 ms (97.37%) | Memory: 44.4 MB (99.32%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,41 +1,28 @@
 class MinStack {
-    private static ArrayList<Integer> stack ;
-    private static int size;
-    private static int minVal;
-
+    Stack<Integer> st;
+    int min = Integer.MAX_VALUE;
     public MinStack() {
-        this.stack= new ArrayList<>();
-        this.size=0;
-        this.minVal=Integer.MAX_VALUE;
+        this.st =new Stack<>();
     }
+    
     public void push(int val) {
-        size++;
-        this.minVal = Math.min(val,minVal);
-        this.stack.add(val);
-        return;
+        if(val<=min){
+            st.push(min);
+            min = val;
+        }
+        st.push(val);
     }
     
     public void pop() {
-        newMin(this.stack.remove(size-1));
-        size--;
-        return;
+        if(st.pop()==min)min=st.pop();
     }
     
     public int top() {
-        return this.stack.get(size-1);
+        return st.peek();
     }
     
     public int getMin() {
-        return minVal;
-    }
-    private void newMin(int val){
-        if(minVal==val){
-            this.minVal=Integer.MAX_VALUE;
-            for(int i=0;i<size-1;i++){
-                this.minVal= Math.min(this.stack.get(i),minVal);
-            }
-        }
-        return;
+        return min;
     }
 }
 
